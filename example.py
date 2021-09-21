eb283fec76d99face3f21a5ea44a262ff032f22c
taowenyin <wenyin.tao@163.com>
2021-09-21 22:57:14 +0800

    添加模型生成

--- example.py
+++ example.py
@@ -1,3 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+from torchvision import models
+
+
 if __name__ == '__main__':
-    print(399 // 20)
-    print((399 + 20 - 1) // 20)
+    # 创建DeepLabV3
+    deeplab = models.segmentation.deeplabv3_resnet50(pretrained=True)
+
+    # 获取BackBone
+    backbone = deeplab.backbone
+    # 获取ASPP
+    aspp = deeplab.classifier[0]
+
+    # layers = [backbone, aspp]
+    # enc = nn.Sequential(*layers)
+    #
+    conv1 = deeplab.backbone.conv1
+    bn1 = deeplab.backbone.bn1
+    relu = deeplab.backbone.relu
+    maxpool = deeplab.backbone.maxpool
+    layer1 = deeplab.backbone.layer1
+
+    # input = torch.randn(1, 3, 224, 224)
+    input = torch.randn(2, 3, 352, 480)
+
+    print(input.shape)
+    print('==============================')
+
+    output = backbone(input)['out']
+    print(output.shape)
+    print('===========backbone==========')
+
+    aspp_output = aspp(output)
+    print(aspp_output.shape)
+    print('===========aspp1==========')
+
+    aspp_output = F.interpolate(aspp_output, size=(int(math.ceil(input.size()[-2] / 4)), int(math.ceil(input.size()[-1] / 4))),
+                                mode='bilinear', align_corners=True)
+    print(aspp_output.shape)
+    print('===========aspp2==========')
+
+    output = conv1(input)
+    output = bn1(output)
+    output = relu(output)
+    output = maxpool(output)
+    output = layer1(output)
+    print(output.shape)
+    print('=========layer1==============')
+
+    conv2 = nn.Conv2d(256, 256, kernel_size=1, bias=False)
+    bn2 = nn.BatchNorm2d(256)
+    relu2 = nn.ReLU()
+
+    output = conv2(output)
+    output = bn2(output)
+    output = relu2(output)
+    print(output.shape)
+    print('=========conv2==============')
+
+    output = torch.cat((aspp_output, output), dim=1)
+    print(output.shape)
+    print('=========cat==============')
+
+    # print(enc.backbone)
+    # print('==============================')
+    # print(enc.classifier[0])
+    # print('==============================')
+    # print(enc)
+
+    # print(aspp.convs)
+    # print(aspp)
+    # print(deeplab)
--- semattlcd/models/models_generic.py
+++ semattlcd/models/models_generic.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 from torchvision import models
 
@@ -18,19 +20,71 @@ class L2Norm(nn.Module):
         return F.normalize(input_data, p=2, dim=self.dim)
 
 
+class FeaturesModel(nn.Module):
+    def __init__(self):
+        super(FeaturesModel, self).__init__()
+
+        # 创建DeepLabV3
+        deeplab = models.segmentation.deeplabv3_resnet50(pretrained=True)
+
+        # 获取BackBone
+        self.backbone = deeplab.backbone
+
+        self.conv1 = self.backbone.conv1
+        self.bn1 = self.backbone.bn1
+        self.relu = self.backbone.relu
+        self.maxpool = self.backbone.maxpool
+        self.layer1 = self.backbone.layer1
+
+        # 获取ASPP
+        self.aspp = deeplab.classifier[0]
+
+        # Low Level要经过1x1的卷积
+        self.conv2 = nn.Conv2d(256, 256, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        # 经过backbone的Layer1获得1/4的特征
+        output = self.conv1(x)
+        output = self.bn1(output)
+        output = self.relu(output)
+        output = self.maxpool(output)
+        low_level_features = self.layer1(output)
+
+        # 经过backbone和ASPP获取1/8特征
+        output = self.backbone(x)
+        aspp_output = self.aspp(output)
+
+        # 把ASPP变大2倍，与low_level_features大小相同，为1/4
+        aspp_output = F.interpolate(aspp_output, size=(int(math.ceil(x.size()[-2] / 4)), int(math.ceil(x.size()[-1] / 4))),
+                          mode='bilinear', align_corners=True)
+
+        low_level_features = self.conv2(low_level_features)
+        low_level_features = self.bn2(low_level_features)
+        low_level_features = self.relu2(low_level_features)
+
+        # 拼接ASPP和low_level_features后的结果
+        output = torch.cat((aspp_output, low_level_features), dim=1)
+
+        return output
+
+
 def get_backend():
-    enc_dim = 512
-    enc = models.vgg16()
-    layers = list(enc.features.children())[:-2]
-    # only train conv5_1, conv5_2, and conv5_3 (leave rest same as Imagenet trained weights)
-    for layer in layers[:-5]:
-        for p in layer.parameters():
-            p.requires_grad = False
+    # 创建DeepLabV3
+    deeplab = models.segmentation.deeplabv3_resnet50(pretrained=True)
+    # 获取BackBone
+    backbone = deeplab.backbone
+    # 获取ASPP
+    aspp = deeplab.classifier[0]
+
+    layers = [backbone, aspp]
     enc = nn.Sequential(*layers)
-    return enc_dim, enc
+
+    return backbone, enc
 
 
-def get_model(encoder, encoder_dim, config, append_pca_layer=False):
+def get_model(encoder, encoder_dim, config):
     nn_model = nn.Module()
     nn_model.add_module('encoder', encoder)
 
